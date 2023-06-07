f28508be2ba376a9cceb91081e7a337f959646a8
Ravi Prakash <120682913+raviprakash1998@users.noreply.github.com>
2023-06-07 15:36:54 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/App.css';
 const App = () => {
 
-  
+  const [count,setCOunt]=useState(1);
+const handleInc=() => {
+  setCOunt(count+1);
+};
+const handleDec=() => {
+  setCOunt(count-1);
+};
   return (
     <div id="main">
+     <button id="increment"onClick={handleInc}>increment</button>
+      { count%3==0 && count%5==0 && <div className='fizzbuzz' id="counter">{count}</div> || count%3==0 && <div className='fizz' id="counter">{count}</div> || count%5==0 && <div className='buzz' id="counter">{count}</div> || <div className='normal' id="counter">{count}</div> }
+    <button id="decrement" onClick={handleDec}>decrement</button>
 
     </div>
   )
